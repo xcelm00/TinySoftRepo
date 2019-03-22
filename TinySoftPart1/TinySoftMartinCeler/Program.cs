c7208f94852761a6d401d66c4eeb7080f354a458
Martin Celer <martinceler@gmail.com>
2019-03-22 20:35:03 +0100

    Initital2

---
+++ TinySoftPart1/TinySoftMartinCeler/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinySoftMartinCeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Random rand = new Random();
+            int winNumber = 0;
+            int chance = rand.Next(1, 100);
+
+            #region InitiateGame
+            switch (chance)
+            {
+                case int n when chance > 0 && chance <= 25:
+                    winNumber = 10;
+                    break;
+                case int n when chance > 25 && chance <= 47:
+                    winNumber = 9;
+                    break;
+                case int n when chance > 47 && chance <= 62:
+                    winNumber = 8;
+                    break;
+                case int n when chance > 62 && chance <= 73:
+                    winNumber = 7;
+                    break;
+                case int n when chance > 73 && chance <= 82:
+                    winNumber = 6;
+                    break;
+                case int n when chance > 82 && chance <= 89:
+                    winNumber = 5;
+                    break;
+                case int n when chance > 89 && chance <= 94:
+                    winNumber = 4;
+                    break;
+                case int n when chance > 94 && chance <= 97:
+                    winNumber = 3;
+                    break;
+                case int n when chance > 97 && chance <= 99:
+                    winNumber = 2;
+                    break;
+                case int n when chance > 99 && chance <= 100:
+                    winNumber = 1;
+                    break;
+            }
+            #endregion
+
+            Console.WriteLine("And the number is " + winNumber + "!!!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TinySoftPart2/TinySoftPart2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinySoftPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double kredit = 10000; //set for any value
+            int multiplier = 0;
+            double vyhra = 0;
+            Random rand = new Random();
+
+            //Please change the location of the logging file to desired location
+            string path = @"C:\Users\Martin\Desktop\csv\log.txt"; //location of logging file
+
+            #region CheckForFile
+            using (var myFile = File.OpenWrite(path))             //check if file exists if it doesn't then create it
+
+            {
+            }
+            #endregion
+            #region CreateDelimiter
+            using (var w = new StreamWriter(path, true))
+            {
+                w.WriteLine("-------------------------------");
+                w.WriteLine("New Game");
+                w.WriteLine("-------------------------------");
+            }
+            #endregion
+            #region InitiateGame
+            for (int i = 0; i < 9999; i++)
+            {
+                int helper = rand.Next(1, 100);
+
+                //double bet = 0.05 * kredit; //5% kreditu
+                double bet = 500; //500
+                if (kredit - bet < 0)
+                {
+                    Console.WriteLine("Game Over after " + i + " spins");
+                    break;
+                }
+                kredit -= bet;
+
+                switch (helper)
+                {
+                    case int n when helper > 0 && helper<= 80:
+                        multiplier = 0;
+                        break;
+                    case int n when helper> 80 && helper<= 90:
+                        multiplier = 1;
+                        break;
+                    case int n when helper> 90 && helper<= 97:
+                        multiplier = 10;
+                        break;
+                    case int n when helper> 97 && helper<= 100:
+                        multiplier = 100;
+                        break;
+                }
+                kredit += multiplier * bet;
+                //Log the result
+                using (var w = new StreamWriter(path, true))
+                {
+                    w.WriteLine(i + " " + kredit + " " + bet + " " + vyhra + " " + multiplier * bet + " " + multiplier);
+                }
+            }
+            #endregion
+
+        }
+    }
+
+}
