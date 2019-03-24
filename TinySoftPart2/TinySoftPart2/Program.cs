29b127e475bc2e9d8d62e5c1504ebc5c397be78b
Martin Celer <martinceler@gmail.com>
2019-03-24 07:37:41 +0100

    Final

--- TinySoftPart2/TinySoftPart2/Program.cs
+++ TinySoftPart2/TinySoftPart2/Program.cs
@@ -13,21 +13,19 @@ namespace TinySoftPart2
     {
         static void Main(string[] args)
         {
-
             double kredit = 10000; //set for any value
-            int multiplier = 0;
             double vyhra = 0;
             Random rand = new Random();
 
             //Please change the location of the logging file to desired location
-            string path = @"C:\Users\Martin\Desktop\csv\log.txt"; //location of logging file
+            string path = @"C:\Users\Martin\Desktop\log.txt"; //location of logging file
 
             #region CheckForFile
             using (var myFile = File.OpenWrite(path))             //check if file exists if it doesn't then create it
-
             {
             }
             #endregion
+   
             #region CreateDelimiter
             using (var w = new StreamWriter(path, true))
             {
@@ -36,6 +34,7 @@ namespace TinySoftPart2
                 w.WriteLine("-------------------------------");
             }
             #endregion
+            
             #region InitiateGame
             for (int i = 0; i < 9999; i++)
             {
@@ -50,31 +49,20 @@ namespace TinySoftPart2
                 }
                 kredit -= bet;
 
-                switch (helper)
-                {
-                    case int n when helper > 0 && helper<= 80:
-                        multiplier = 0;
-                        break;
-                    case int n when helper> 80 && helper<= 90:
-                        multiplier = 1;
-                        break;
-                    case int n when helper> 90 && helper<= 97:
-                        multiplier = 10;
-                        break;
-                    case int n when helper> 97 && helper<= 100:
-                        multiplier = 100;
-                        break;
-                }
-                kredit += multiplier * bet;
+                WheelOfFortune.Spin(helper);
+
+                kredit += WheelOfFortune.Multiplier * bet;
+                
                 //Log the result
                 using (var w = new StreamWriter(path, true))
                 {
-                    w.WriteLine(i + " " + kredit + " " + bet + " " + vyhra + " " + multiplier * bet + " " + multiplier);
+                    w.WriteLine(i + " " + kredit + " " + bet + " " + vyhra + " " + WheelOfFortune.Multiplier * bet + " " + WheelOfFortune.Multiplier);
                 }
             }
             #endregion
-
         }
     }
+
+
 
 }
---
+++ TinySoftPart2/TinySoftPart2/WheelOfFortune.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinySoftPart2
+{
+    public static class WheelOfFortune
+    {
+        public static int Multiplier;
+        public static int Spin(int helper)
+        {
+            switch (helper)
+            {
+                case int n when helper > 0 && helper <= 80:
+                    Multiplier = 0;
+                    break;
+                case int n when helper > 80 && helper <= 90:
+                    Multiplier = 1;
+                    break;
+                case int n when helper > 90 && helper <= 97:
+                    Multiplier = 10;
+                    break;
+                case int n when helper > 97 && helper <= 100:
+                    Multiplier = 100;
+                    break;
+            }
+            return Multiplier;
+        }
+    }
+}
